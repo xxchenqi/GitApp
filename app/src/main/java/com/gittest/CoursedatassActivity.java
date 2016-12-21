e567683d7833a5532c9356ba5f56f85553099ab9
cq <812046652@qq.com>
2016-12-21 22:33:50 +0800

    重命名

---
+++ app/src/main/java/com/gittest/CoursedatassActivity.java
@@ -0,0 +1,15 @@
+package com.gittest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/12/21 0021.
+ */
+
+public class CoursedatassActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
