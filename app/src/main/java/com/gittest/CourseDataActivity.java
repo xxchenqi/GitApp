b88e1263e69c4c2d5371f27ab9c7ccf6950fe454
cq <812046652@qq.com>
2016-12-21 22:43:42 +0800

    重命名3

--- app/src/main/java/com/gittest/CourseDataActivity.java
+++ app/src/main/java/com/gittest/CourseDataActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
  * Created by Administrator on 2016/12/21 0021.
  */
 
-public class CoursedataActivitya extends Activity {
+public class CourseDataActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
